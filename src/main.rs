fcf403dc2234718c4a11deda6599d0e201522461
emm312 <emmtbm55@gmail.com>
2023-01-11 12:46:39 +1100

    remove shitty commit and add test case

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use codegem::ir::*;
+use codegem::arch::urcl::UrclSelector;
+use codegem::arch::x64::X64Selector;
+use codegem::arch::rv64::RvSelector;
+use codegem::regalloc::RegAlloc;
+
+
+
+
+fn main() {
+    let mut builder = ModuleBuilder::default().with_name("test");
+    let main_func = builder.new_function("main", &[], &Type::Void);
+    builder.switch_to_function(main_func);
+    let entry = builder.push_block().unwrap();
+    builder.switch_to_block(entry);
+    let val = builder.push_instruction(&Type::Integer(true, 32), Operation::Integer(
+        true,
+        69u32.to_le_bytes().to_owned().to_vec())).unwrap();
+    
+    let val2 = builder.push_instruction(&Type::Integer(true, 32), Operation::Integer(
+            true,
+            69u32.to_le_bytes().to_owned().to_vec())).unwrap();
+    
+    let val3 = builder.push_instruction(&Type::Integer(true, 32), Operation::Add(val, val2)).unwrap();
+    builder.set_terminator(Terminator::Return(val3));
+    let module = builder.build();
+    let mut vcode = module.lower_to_vcode::<_, UrclSelector>();
+    vcode.allocate_regs::<RegAlloc>();
+    vcode.emit_assembly();
+    
+}
